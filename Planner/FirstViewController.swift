6c0af517635789348fd316d55224face27e2375a
Jiating Su <jiating.su@stonybrook.edu>
2020-06-22 18:11:12 -0400

    Initial Commit

---
+++ Planner/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Planner
+//
+//  Created by 郑植 on 6/22/20.
+//  Copyright © 2020 CSE 390. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Planner/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Planner
+//
+//  Created by 郑植 on 6/22/20.
+//  Copyright © 2020 CSE 390. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
