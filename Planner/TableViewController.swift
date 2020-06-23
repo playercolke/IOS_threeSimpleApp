64aed68206039c772f7f93c02f3f72fd3eadac16
Jiating Su <jiating.su@stonybrook.edu>
2020-06-23 18:51:23 -0400

    Table Almost Done, working on fixing detail

--- Planner/TableViewController.swift
+++ Planner/TableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class TableViewController: UIViewController {
+class TableViewController: UITableViewController {
 
     
     var events:[NSManagedObject] = []
@@ -25,6 +25,102 @@ class TableViewController: UIViewController {
         loadDataFromDatabase()
         tableView.reloadData()
     }
+    
+    func loadDataFromDatabase() {
+        //Set up Core Data Context
+        let context = appDelegate.persistentContainer.viewContext
+        //Set up Request
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Event")
+        do {
+            events = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    // MARK: - Table view data source
 
+      override func numberOfSections(in tableView: UITableView) -> Int {
+          return 1
+      }
+
+      override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return events.count
+      }
+
+     
+      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "EventsCell", for: indexPath)
+          
+          // Configure the cell...
+          let event = events[indexPath.row] as? Event
+          cell.textLabel?.text = event?.eventName
+          cell.detailTextLabel?.text = event?.classes
+          cell.accessoryType = .detailDisclosureButton
+          return cell
+      }
+    
+      override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          let selectedEvent = events[indexPath.row] as? Event
+          let name = selectedEvent!.eventName!
+          let actionHandler = { (action:UIAlertAction!) -> Void in
+              let storyboard = UIStoryboard(name: "Main", bundle: nil)
+              let controller = storyboard.instantiateViewController(withIdentifier: "EventController")
+                  as? EventViewController
+              controller?.currentEvent = selectedEvent
+              self.navigationController?.pushViewController(controller!, animated: true)
+          }
+          
+          let alertController = UIAlertController(title: "Contact selected",
+                                                  message:  "Selected row: \(indexPath.row) (\(name))",
+              preferredStyle: .alert)
+          
+          let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+          let actionDetails = UIAlertAction(title: "Show Details", style: .default, handler: actionHandler)
+          alertController.addAction(actionCancel)
+          alertController.addAction(actionDetails)
+          present(alertController, animated: true, completion: nil)
+      }
+
+      /*
+      // Override to support conditional editing of the table view.
+      override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+          // Return false if you do not want the specified item to be editable.
+          return true
+      }
+      */
+
+      override func tableView(_ tableView: UITableView,
+                              commit editingStyle: UITableViewCell.EditingStyle,
+                              forRowAt indexPath: IndexPath) {
+          if editingStyle == .delete {
+              // Delete the row from the data source
+              let event = events[indexPath.row] as? Event
+              let context = appDelegate.persistentContainer.viewContext
+              context.delete(event!)
+              do {
+                  try context.save()
+              }
+              catch  {
+                  fatalError("Error saving context: \(error)")
+              }
+              loadDataFromDatabase()
+              tableView.deleteRows(at: [indexPath], with: .fade)
+          } else if editingStyle == .insert {
+              // Create a new instance of the appropriate class, insert it into the array,
+              //and add a new row to the table view
+          }
+      }
+
+      
+      
+      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+          if segue.identifier == "EditEvent" {
+              let eventController = segue.destination as? EventViewController
+              let selectedRow = self.tableView.indexPath(for: sender as! UITableViewCell)?.row
+              let selectedEvent = events[selectedRow!] as? Event
+              eventController?.currentEvent = selectedEvent!
+          }
+      }
 }
 
